f89bf84bfbccd257e90944b480193d786e44774e
wyatt522 <wyattred1@gmail.com>
2024-04-23 14:55:04 -0400

    added position versus velocity controller

--- src/SAC.m
+++ src/SAC.m
@@ -10,8 +10,8 @@ classdef SAC < handle
         maxVels(3,1) double;
         output_count(1,1) double;
         target_time(1,1) double;
-        hard_follow_timer(1,1) double;
         jump_ahead_level(1,1) double;
+        fast_k(4, 12) double;
 
     end
 
@@ -21,15 +21,18 @@ classdef SAC < handle
             obj.timeStep = 0.01;
             obj.maxVels = [0.15; 0.15; 1];
             position = [1,0,0];
-            Q = diag([5,5,5,3,3,3,4,4,4,1.5,1.5,1.5]);
+            Q = diag([4, 4,4,15, 15, 4,1.5,1.5,1.5,1.5,1.5,1.5]);
+            fast_Q = diag([10,10,10, 8,8, 4,1.5,1.5,1.5,1.5,1.5,1.5]);
             R = 3*eye(4);
             [A, B] = linearize_quad(quadrotor, position);
             [K,~, ~] = lqr(A,B,Q,R);
+            [fast_K,~, ~] = lqr(A,B,fast_Q,R);
             obj.k = K;
             obj.output_count = 0;
             obj.prev_coeffs = zeros(3, 8);
             obj.target_time = -1;
             obj.jump_ahead_level = 4.0;
+            obj.fast_k = fast_K;
 
         end
 
@@ -41,33 +44,37 @@ classdef SAC < handle
                 % determine trajectory of uav
                 [~, coeff_size] = size(self.prev_coeffs);
                 coeffs = self.solveCoeffs(y, coeff_size - 1);
-                % collect data first 200 timesteps
-                if self.output_count > 100
+                % collect data first 100 timesteps
+                
+                error_vec = z(1:3) - y;
+                error_mag = norm(error_vec);
+
+                if (self.output_count > 100 && error_mag > 1)
                     % no target time, attempt to aquire one
                     if (self.target_time == -1)
                         % check to find target, go straight to UAV
                         self.target_time = self.jump_ahead_level;
-                        self.jump_ahead_level = self.jump_ahead_level + 0.2;
-                        r(1:3) = y(1:3);
-                    elseif (self.target_time == 0)
-                        self.hard_follow_timer = self.hard_follow_timer + 1;
-                        r(1:3) = y(1:3);
-                        if self.hard_follow_timer == 100
-                            self.hard_follow_timer = 0;
-                            self.target_time = -1;
-                        end
+                        self.jump_ahead_level = self.jump_ahead_level + 0.1;
                     else
                         % update for time passing, and go to the expected
                         % location
-                        r(1:3) = solvePoly(coeffs, self.target_time);
                         self.target_time = max(self.target_time - self.timeStep, 0);
+                        if (self.target_time == 0)
+                            self.target_time = -1;
+                        end
                     end
+                    r(1:3) = solvePoly(coeffs, self.target_time);
                 else
                     % update data collection timer, go straight to UAV
                     self.output_count = self.output_count + 1;
                     r(1:3) = y(1:3);
                 end
-                u = repmat(self.u0, [4,1]) + self.k*(r - z);
+                if error_mag < 1
+                    temp_k = self.fast_k;
+                else
+                    temp_k = self.k;
+                end
+                u = repmat(self.u0, [4,1]) + temp_k*(r - z);
             else
                 home = zeros(12, 1);
                 home(3) = 1;
