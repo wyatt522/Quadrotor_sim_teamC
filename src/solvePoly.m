b8b43d2a44d02cbfd6b5073f96bf509de3a99857
wyatt522 <wyattred1@gmail.com>
2024-04-28 17:22:47 -0400

    removed unneccessary files

--- src/solvePoly.m
+++
@@ -1,8 +0,0 @@
-function futurePose = solvePoly(coeffs, t)
-    [~, col] = size(coeffs);
-    time = [];
-    for i = (col-1):-1:0
-        time = [time; (t^(i))];
-    end
-    futurePose = coeffs*time;
-end
--- src/solveQuintic.m
+++
@@ -1,3 +0,0 @@
-function futurePose = solveQuintic(coeffs, t)
-    futurePose = coeffs*[t^5; t^4; t^3; t^2; t; 1];
-end
